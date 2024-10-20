794213d78a85627a2f1f0b097ad218492b92d3c6
sanskar sharma <sanskarsharma6261@gmail.com>
2024-10-21 01:45:10 +0530

    some new changes add

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    apps:[
+        {
+            name:"Velora",
+            script:"yarn",
+            args:"run dev",
+            env:{
+                NODE_ENV:"development"
+            }
+
+        }
+    ]
+}
